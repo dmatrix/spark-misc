c5111699c447ab521f9c93872d988d28bcd9b6f8
Jules Damji <dmatrix@comast.net>
2025-01-21 16:00:33 -0800

    tested dbconnect with SQL pyspark scripts
    Signed-off-by: Jules Damji <dmatrix@comast.net>

--- src/py/sc/sql/df_example_1.py
+++ src/py/sc/sql/df_example_1.py
@@ -1,24 +1,37 @@
 #
 # Example used from Learning Spark v2 
 #
+import os
 import sys
 sys.path.append('.')
+
+from src.py.sc.utils.spark_session_cls import SparkConnectSession
+from src.py.sc.utils.spark_session_cls import DatabrckSparkSession
 from src.py.sc.utils.print_utils import print_header, print_seperator
 
-from pyspark.sql import SparkSession
-import os
-
-DATA_FILE="/Users/jules/git-repos/spark-misc/src/py/sc/sql/data/mm_data.csv"
+DATA_FILE = "/Users/jules/git-repos/spark-misc/src/py/sc/sql/data/mm_data.csv"
+STORAGE_LOCATION = "mnn_count_table"
 
 if __name__ == "__main__":
-    # let's stop any existing SparkSession if running at all
-    SparkSession.builder.master("local[*]").getOrCreate().stop()
+    spark = None
+    # Create a new session with Spark Connect mode={"dbconnect", "connect", "classic"}
+    if len(sys.argv) <= 1:
+        args = ["dbconnect", "classic", "connect"]
+        print(f"Command line must be one of these values: {args}")
+        sys.exit(1)  
+    mode = sys.argv[1]
+    print(f"++++ Using Spark Connect mode: {mode}")
+    
+    # create Spark Connect type based on type of SparkSession you want
+    if mode == "dbconnect":
+        cluster_id = os.environ.get("clusterID")
+        assert cluster_id
+        DATA_FILE = "dbfs:/databricks-datasets/learning-spark-v2/mnm_dataset.csv"
+        spark = spark = DatabrckSparkSession().get()
+    else:
+        spark = SparkConnectSession(remote="local[*]", mode=mode,
+                                app_name="PySpark DataFrame API MnMCount Example 1").get()
 
-    # Create a new session with Spark Connect
-    spark = (SparkSession.builder
-             .remote("local[*]")
-             .appName("PySpark DataFrame API MnMCount Example 1")
-             .getOrCreate())
     
     # Ensure we are conneccted to the spark session
     assert("<class 'pyspark.sql.connect.session.SparkSession'>" == str(type((spark))))
@@ -38,34 +51,40 @@ if __name__ == "__main__":
 
     # aggregate count of all colors and groupBy state and color
     # orderBy descending order
-
+    print_header("GROUPBY, AGGREGATE BY STATES AND COLOUR: ")
     count_mnm_df = (mnm_df.select("State", "Color", "Count")
                     .groupBy("State", "Color")
                     .sum("Count")
-                    .orderBy("sum(Count)", ascending=False))
+                    .orderBy("sum(Count)", ascending=False).alias("sum_count"))
+
+    count_mnm_df = count_mnm_df.withColumnRenamed("sum(Count)", "sum_count")
 
     # show all the resulting aggregation for all the Sates and colors
-    print_header("GROUPBY, AGGREGATE BY STATES AND COLOUR: ")
-    count_mnm_df.show(n=60, truncate=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
     print_seperator()
 
     # find the aggregate count for California by filtering
+    # show the resulting aggregation for California
+    print_header("FILTER BY CALIFORINIA STATE")
     ca_count_mnm_df = (mnm_df.select("*")
                        .where(mnm_df.State == 'CA')
                        .groupBy("State", "Color")
                        .sum("Count")
-                       .orderBy("sum(Count)", ascending=False))
+                       .orderBy("sum(Count)", ascending=False).alias("sum_ca_count"))
 
-    # show the resulting aggregation for California
-    print_header("FILTER BY CALIFORINIA STATE")
+    ca_count_mnm_df = ca_count_mnm_df.withColumnRenamed("sum(Count)","sum_ca_count")
     ca_count_mnm_df.show(n=10, truncate=False)
     print_seperator(size=10)
 
-    # save as managed table
-    print_header("SAVE AS A MANAGED TABLE:")
-    ca_count_mnm_df.write.mode("overwrite").save("/tmp/tables/mnn_count_table")
-    print(os.listdir("/tmp/tables/mnn_count_table"))
+    print_header(f"DROPING TABLE {STORAGE_LOCATION} IF EXSITS...")
+    spark.sql(f"DROP TABLE IF EXISTS {STORAGE_LOCATION}")
+    print_header(f"SAVE DATAFRAME AS A MANAGED TABLE:{STORAGE_LOCATION}")
+    ca_count_mnm_df.write.option("mode", "overwrite").saveAsTable(STORAGE_LOCATION)
     print_seperator(size=10)
+
+    print_header("READ DATAFRAME FROM A MANAGED TABLE")
+    df = spark.sql(f"select (*) from {STORAGE_LOCATION}")
+    df.show(n=10, truncate=False)
+
     spark.stop()
           
--- src/py/sc/sql/sql_example_1.py
+++ src/py/sc/sql/sql_example_1.py
@@ -1,23 +1,43 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import count
-
 # Part of the code borrowed from SampleExamples.com for PySpark and modified
 # for this test
-
+import os
 import sys
 sys.path.append('.')
+
+from pyspark.sql.functions import count
+
+from src.py.sc.utils.spark_session_cls import SparkConnectSession
+from src.py.sc.utils.spark_session_cls import DatabrckSparkSession
 from src.py.sc.utils.print_utils import print_header
 import time
 
-DATA_FILE="/Users/jules/git-repos/spark-misc/src/py/sc/sql/data/zipcodes.csv"
+DATA_FILE = "/Users/jules/git-repos/spark-misc/src/py/sc/sql/data/zipcodes.csv"
+DATA_COLUMNS = ["RecordNumber", "Country", "City", "Zipcode", "State"]
+DATA_RECORDS = [(1,"US","PARC PARQUE",704,"PR"),
+                (2,"US","PASEO COSTA DEL SUR",704,"PR"),
+                (10,"US","BDA SAN LUIS",709,"PR"),
+                (49347,"US","HOLT",32564,"FL"),
+                (49348,"US","HOMOSASSA",34487,"FL"),
+                (61391,"US","CINGULAR WIRELESS",76166,"TX"),
+                (61392,"US","FORT WORTH",76177,"TX"),
+                (61393,"US","FT WORTH",76177,"TX"),
+                (54356,"US","SPRUCE PINE",35585,"AL"),
+                (76511,"US","ASH HILL",27007,"NC"),
+                (4,"US","URB EUGENE RICE",704,"PR"),
+                (39827,"US","MESA",85209,"AZ"),
+                (39828,"US","MESA",85210,"AZ"),
+                (49345,"US","HILLIARD",32046,"FL"),
+                (49346,"US","HOLDER",34445,"FL"),
+                (3,"US","SECT LANAUSSE",704,"PR"),
+                (54354,"US","SPRING GARDEN",36275,"AL"),
+                (54355,"US","SPRINGVILLE",35146,"AL"),
+                (76512,"US","ASHEBORO",27203,"NC"),
+                (76513,"US","ASHEBORO",27204,"NC")]
 
 def run_dataframe_query(sp, num:int=5):
 
     # Create DataFrame
-    df = (sp.read 
-            .option("header",True) 
-            .csv(DATA_FILE))
-    df.printSchema()
+    df = sp.createDataFrame(DATA_RECORDS).toDF(*DATA_COLUMNS)
     df.show(num)
 
     # Dataframe Select query
@@ -40,14 +60,13 @@ def run_dataframe_query(sp, num:int=5):
     
     print_header("DATAFRAME SELECT STATE COUNT FROM ZIPCODES:")
     (df.groupBy("state").agg(count("*").alias("count")).show())
+
+    return df
     
-def run_sql_query(sp):
+def run_sql_query(sp, zdf):
     # Create TempView SQL table
-    (spark.read 
-            .option("header",True) 
-            .csv(DATA_FILE) 
-            .createOrReplaceTempView("Zipcodes"))
-            
+
+    zdf.createOrReplaceTempView("ZIPCODES")
     print_header("SQL SELECT COLUMNS from ZIPCODES:")
     (sp.sql("SELECT  country, city, zipcode, state FROM ZIPCODES")
         .show(5))
@@ -68,23 +87,29 @@ def run_sql_query(sp):
         .show())
 
 if __name__ == "__main__":
-
-    # let's top any existing SparkSession if running at all
-    SparkSession.builder.master("local[*]").getOrCreate().stop()
-
-    # Create SparkSession
-    spark = (SparkSession
-                .builder
-                .remote("local[*]")
-                .appName('PySpark SQL EXAMPLE') 
-                .getOrCreate())
+    spark = None
+    # Create a new session with Spark Connect mode={"dbconnect", "connect", "classic"}
+    if len(sys.argv) <= 1:
+        args = ["dbconnect", "classic", "connect"]
+        print(f"Command line must be one of these values: {args}")
+        sys.exit(1)  
+    mode = sys.argv[1]
+    print(f"++++ Using Spark Connect mode: {mode}")
+    
+    # create Spark Connect type based on type of SparkSession you want
+    if mode == "dbconnect":
+        cluster_id = os.environ.get("clusterID")
+        assert cluster_id
+        spark = spark = DatabrckSparkSession().get()
+    else:
+        spark = SparkConnectSession(remote="local[*]", mode=mode,
+                                app_name="PySpark SQL EXAMPLE").get()
     
     # Ensure we are conneccted to the spark session
     assert("<class 'pyspark.sql.connect.session.SparkSession'>" == str(type((spark))))
     print(f"+++++Making sure it's using SparkConnect session:{spark}+++++")
                         
-    run_dataframe_query(spark)
-    run_sql_query(spark)
+    df = run_dataframe_query(spark)
+    run_sql_query(spark, df)
     
-    time.sleep(30) # for examining the SparkUI for jobs/stages/tasks generated from SparkConnect session
     spark.stop()
